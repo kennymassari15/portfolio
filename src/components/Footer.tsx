4261a5fa4c06e29489eb3ba560867962c8e77871
Kenny Massari <kenny.massari315@gmail.com>
2023-11-19 15:13:58 -0600

    init

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export const Footer = () => {
+  return (
+    <div className="flex flex-row justify-between bg-[#1e1e1e] rounded-lg py-4 px-5 shadow-2xl">
+      <div className="text-white">Kenny Massari</div>
+      <a
+        href="https://gitlab.com/kenny.massari"
+        target={"_blank"}
+        rel="noreferrer"
+        className="text-green-300"
+      >
+        GitLab
+      </a>
+    </div>
+  );
+};
